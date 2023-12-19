533ccc265d7e7a22737d6c97dda3b529a9035998
revanAlihummatov <ravanalihummatov@gmail.com>
2023-12-19 11:55:35 +0300

    test

---
+++ LeetCodeMeseleler/src/leetcodeMeseleler/RDFSA.java
@@ -0,0 +1,47 @@
+package leetcodeMeseleler;
+
+public class RDFSA {
+    public static void main(String[] args) {
+        RDFSA rdfsa = new RDFSA();
+        int[] nums = {1, 1, 2};
+        int result = rdfsa.removeDuplicates(nums);
+        System.out.println(result);
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == nums[i]) {
+                    int[] result = removeElement(nums, j - 1);
+                    k = result.length;
+                }
+            }
+        }
+        return k;
+    }
+
+    public static int[] removeElement(int[] nums, int val) {
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            int[] newArr = new int[nums.length - 1];
+            int newIndex = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (i != index) {
+                    newArr[newIndex++] = nums[i];
+                }
+            }
+            return newArr;
+        }
+
+        return nums;
+    }
+
+}
