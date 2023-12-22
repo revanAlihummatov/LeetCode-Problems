ba5e1ca0749b4338c0590ad4783c5ab0c89e99c6
revanAlihummatov <ravanalihummatov@gmail.com>
2023-12-22 18:21:16 +0300

    test

---
+++ LeetCodeMeseleler/src/leetcodeMeseleler/FindFIrstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,37 @@
+package leetcodeMeseleler;
+
+public class FindFIrstAndLastPositionOfElementInSortedArray {
+    public static void main(String[] args) {
+        FindFIrstAndLastPositionOfElementInSortedArray ffalpoeisa = new FindFIrstAndLastPositionOfElementInSortedArray();
+        int target = 3;
+        int[] nums = {3,3,3};
+        int[] result = ffalpoeisa.searchRange(nums,target);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+        }
+    }
+    public int[] searchRange(int[] nums, int target) {
+        boolean b = false;
+        int[] arr = {-1,-1};
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (arr[0] == -1){
+                    arr[0] = i;
+                }
+                arr[1] = i;
+
+            }
+
+        }
+        if (b){
+            if (arr[1] == 0){
+                arr[1] = arr[0];
+            }
+            return arr;
+        }
+
+
+        return arr;
+    }
+}
