d91d476d07a641a95a46981c6fa1743d6ef9a3f4
revanAlihummatov <ravanalihummatov@gmail.com>
2023-12-23 14:46:49 +0300

    test

---
+++ LeetCodeMeseleler/src/leetcodeMeseleler/PlusOne.java
@@ -0,0 +1,24 @@
+package leetcodeMeseleler;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        PlusOne po = new PlusOne();
+        int[] digits = {1,2,3};
+        System.out.println(Arrays.toString(po.plusOne(digits)));
+    }
+
+    public int[] plusOne(int[] digits) {
+        long number = Integer.parseInt(Arrays.toString(digits).replaceAll("\\D", ""));
+        number++;
+        String numberString = String.valueOf(number);
+
+        digits = new int[numberString.length()];
+
+        for (int i = 0; i < numberString.length(); i++) {
+            digits[i] = numberString.charAt(i) - '0';
+        }
+        return digits;
+    }
+}
