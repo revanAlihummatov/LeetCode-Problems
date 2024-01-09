0ffcf3a70ccc93036855f78bc82ce73bbc1f0ff0
revanAlihummatov <ravanalihummatov@gmail.com>
2024-01-09 10:05:28 +0300

    test

---
+++ LeetCodeMeseleler/src/leetcodeMeseleler/RunnigSumOf1dArray.java
@@ -0,0 +1,22 @@
+package leetcodeMeseleler;
+
+import java.util.Arrays;
+
+public class RunnigSumOf1dArray {
+    public static void main(String[] args) {
+        RunnigSumOf1dArray runnigSumOf1dArray = new RunnigSumOf1dArray();
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(runnigSumOf1dArray.runningSum(nums)));
+    }
+    public int[] runningSum(int[] nums) {
+        int[] nums2 = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int val = 0;
+            for (int j = i; j >= 0; j--) {
+                val = val + nums[j];
+            }
+            nums2[i] = val;
+        }
+        return nums2;
+    }
+}
