113210ccd92a6e40c6a03202ccac7323fbc5c840
revanAlihummatov <ravanalihummatov@gmail.com>
2024-01-20 11:31:26 +0300

    test

---
+++ LeetCodeMeseleler/src/string/ToLowerCase.java
@@ -0,0 +1,12 @@
+package string;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(toLowerCase(s));
+    }
+    public static String toLowerCase(String s) {
+        String s2 = s.toLowerCase();
+        return s2;
+    }
+}
