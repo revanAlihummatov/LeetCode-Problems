e529794e7d8e874cdd89cc5f52d999bf61d0c782
revanAlihummatov <ravanalihummatov@gmail.com>
2024-03-01 10:25:45 +0300

    test

--- LeetCodeProblems/src/linkedList/MyHashSet.java
+++ LeetCodeProblems/src/linkedList/MyHashSet.java
@@ -1,34 +1,11 @@
 package linkedList;
 
-import java.util.Arrays;
-
-public class DesingHashSet {
-    public static void main(String[] args) {
-        MyHashSet mySet = new MyHashSet();
-        mySet.add(1);
-        mySet.add(2);
-        System.out.println(mySet.contains(1));
-        System.out.println(mySet.contains(3));
-        mySet.add(2);
-        System.out.println(mySet.contains(2));
-        System.out.println(mySet);
-        mySet.remove(2);
-        System.out.println(mySet);
-        System.out.println(mySet.contains(2));
-    }
-}
-
 class MyHashSet {
     private int length = 0;
     private int[] arr = new int[length];
 
     public MyHashSet() {
 
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(arr);
     }
 
     public void add(int key) {
