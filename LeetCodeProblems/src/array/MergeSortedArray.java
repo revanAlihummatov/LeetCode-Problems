df17e477d7a7ec85e4b88130a9eeba0dd8ab8630
revanAlihummatov <ravanalihummatov@gmail.com>
2024-03-20 12:29:58 +0300

    test

--- LeetCodeProblems/src/array/MergeSortedArray.java
+++ LeetCodeProblems/src/array/MergeSortedArray.java
@@ -7,30 +7,31 @@ public class MergeSortedArray {
         MergeSortedArray msa = new MergeSortedArray();
         int m = 3;
         int n = 3;
-        int[] nums1 = {0};
-        int[] nums2 = {1};
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
         msa.merge(nums1,m,nums2,n);
+        System.out.println(Arrays.toString(nums1));
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums12 = new int[n + m];
-        int j = 0;
-        for (int i = 0; i < nums1.length; i++) {
-            if (nums1[i] != 0){
-                nums12[i] = nums1[i];
-                j++;
+
+        int z = 0;
+        for (int i = m; i < nums1.length; i++) {
+            nums1[i] = nums2[z];
+            z++;
+        }
+        //Arrays.sort(nums1);
+        bubbleSort(nums1);
+    }
+    public static void bubbleSort(int[] arr) {
+        int val = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    val = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = val;
+                }
             }
         }
-        for (int i = 0; i < nums2.length; i++) {
-            if (nums2[i] != 0){
-                nums12[j] = nums2[i];
-                j++;
-            }
-        }
-        Arrays.sort(nums12);
-        nums1 = new int[nums12.length];
-        for (int i = 0; i < nums1.length; i++) {
-            nums1[i] = nums12[i];
-        }
-        System.out.println(Arrays.toString(nums1));
     }
 }
