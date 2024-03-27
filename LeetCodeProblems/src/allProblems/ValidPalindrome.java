505082eca299020896f89f69684f3de654612bb4
revanAlihummatov <ravanalihummatov@gmail.com>
2024-03-27 11:12:44 +0300

    test

--- LeetCodeProblems/src/allProblems/ValidPalindrome.java
+++ LeetCodeProblems/src/allProblems/ValidPalindrome.java
@@ -2,6 +2,13 @@ package allProblems;
 
 public class ValidPalindrome {
     public static void main(String[] args) {
-        
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
     }
+    public static boolean isPalindrome(String s) {
+        return false;
+    }
+    public static void connectString(String s){
+        System.out.println();
+    }
+
 }
