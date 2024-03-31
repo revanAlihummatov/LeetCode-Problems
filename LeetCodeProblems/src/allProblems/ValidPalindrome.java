8b6d1cfee6ad33babf1b7f2d82f1bb4d41c5cdfb
revanAlihummatov <ravanalihummatov@gmail.com>
2024-03-31 10:38:44 +0300

    test

--- LeetCodeProblems/src/allProblems/ValidPalindrome.java
+++ LeetCodeProblems/src/allProblems/ValidPalindrome.java
@@ -2,13 +2,37 @@ package allProblems;
 
 public class ValidPalindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
     }
+
     public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = removeNonLetters(s);
+        String s2 = reverseString(s);
+        if (s.equals(s2)) return true;
         return false;
     }
-    public static void connectString(String s){
 
+    public static String removeNonLetters(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isLetter(c) || Character.isDigit(c)) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
     }
 
+    public static String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+
+        return reversed.toString();
+    }
 }
